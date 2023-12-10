67d5bac51f7c56e33b2159b66dbadc7bba4c3aaa
Matteus <matteus92@gmail.com>
2023-12-10 22:35:39 +0100

    2023: add day 10 (wip)

--- 2023/day/10/part1.c
+++ 2023/day/10/part1.c
@@ -1,8 +1,21 @@
 #include "common.h"
 #include <assert.h>
+#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEBUG 0
+
+void debug(char *fmt, ...) {
+  (void)fmt;
+#if DEBUG
+  va_list args;
+  va_start(args, fmt);
+  vprintf(fmt, args);
+  va_end(args);
+#endif
+}
 
 typedef struct Position {
   size_t x;
@@ -46,134 +59,77 @@ Position pos(size_t idx, size_t line_len) {
 
 void connected_pipes(char *input, size_t input_len, size_t line_len, Position p,
                      PipePosition *pipe1, PipePosition *pipe2) {
+  // Need to find two pipes, but we might find a third S-pipe which is invalid
+  // only if we find two other pipes.
+  PipePosition pipes[3] = {0};
+  size_t pipe_count = 0;
 
-  bool pipe1_found = false;
-  bool pipe2_found = false;
   Position map_end = pos(input_len - 1, line_len);
+  char pipe = input[idx(p.x, p.y, line_len)];
 
   // Check up
-  if (p.y > 0) {
+  if (p.y > 0 && (pipe == '|' || pipe == 'J' || pipe == 'L' || pipe == 'S')) {
     size_t up_idx = idx(p.x, p.y - 1, line_len);
     if (input[up_idx] == '|' || input[up_idx] == '7' || input[up_idx] == 'F' ||
         input[up_idx] == 'S') {
-      PipePosition **pipe = pipe1_found ? &pipe2 : &pipe1;
-      if (pipe1_found) {
-        pipe2_found = true;
-      } else {
-        pipe1_found = true;
-      }
-      **pipe = (PipePosition){.pos = {p.x, p.y - 1}, .pipe = input[up_idx]};
+      pipes[pipe_count++] =
+          (PipePosition){.pos = {p.x, p.y - 1}, .pipe = input[up_idx]};
     }
   }
 
   // Check down
-  if (p.y < map_end.y) {
+  if (p.y < map_end.y &&
+      (pipe == '|' || pipe == 'F' || pipe == '7' || pipe == 'S')) {
     size_t down_idx = idx(p.x, p.y + 1, line_len);
     if (input[down_idx] == '|' || input[down_idx] == 'J' ||
         input[down_idx] == 'L' || input[down_idx] == 'S') {
-      PipePosition **pipe = pipe1_found ? &pipe2 : &pipe1;
-      if (pipe1_found) {
-        pipe2_found = true;
-      } else {
-        pipe1_found = true;
-      }
-      **pipe = (PipePosition){.pos = {p.x, p.y + 1}, .pipe = input[down_idx]};
+      pipes[pipe_count++] =
+          (PipePosition){.pos = {p.x, p.y + 1}, .pipe = input[down_idx]};
     }
   }
 
   // Check left
-  if (p.x > 0) {
+  if (p.x > 0 && (pipe == '-' || pipe == 'J' || pipe == '7' || pipe == 'S')) {
     size_t left_idx = idx(p.x - 1, p.y, line_len);
     if (input[left_idx] == '-' || input[left_idx] == 'F' ||
         input[left_idx] == 'L' || input[left_idx] == 'S') {
-      PipePosition **pipe = pipe1_found ? &pipe2 : &pipe1;
-      if (pipe1_found) {
-        pipe2_found = true;
-      } else {
-        pipe1_found = true;
-      }
-      **pipe = (PipePosition){.pos = {p.x - 1, p.y}, .pipe = input[left_idx]};
+      pipes[pipe_count++] =
+          (PipePosition){.pos = {p.x - 1, p.y}, .pipe = input[left_idx]};
     }
   }
 
   // Check right
-  if (p.x < map_end.x) {
+  if (p.x < map_end.x &&
+      (pipe == '-' || pipe == 'F' || pipe == 'L' || pipe == 'S')) {
     size_t right_idx = idx(p.x + 1, p.y, line_len);
     if (input[right_idx] == '-' || input[right_idx] == 'J' ||
         input[right_idx] == '7' || input[right_idx] == 'S') {
-      PipePosition **pipe = pipe1_found ? &pipe2 : &pipe1;
-      if (pipe1_found) {
-        pipe2_found = true;
-      } else {
-        pipe1_found = true;
+      pipes[pipe_count++] =
+          (PipePosition){.pos = {p.x + 1, p.y}, .pipe = input[right_idx]};
+    }
+  }
+
+  if (pipe_count == 3) {
+    // Remove the S-pipe
+    for (size_t i = 0; i < 3; i++) {
+      if (pipes[i].pipe == 'S') {
+        pipes[i] = pipes[2];
+        pipe_count--;
+        break;
       }
-      **pipe = (PipePosition){.pos = {p.x + 1, p.y}, .pipe = input[right_idx]};
     }
   }
 
-  if (pipe1_found) {
-    printf("Found pipe %c at %zu, %zu\n", pipe1->pipe, pipe1->pos.x,
-           pipe1->pos.y);
-  }
-  if (pipe2_found) {
-    printf("Found pipe %c at %zu, %zu\n", pipe2->pipe, pipe2->pos.x,
-           pipe2->pos.y);
-  }
-
-  if (!pipe1_found || !pipe2_found) {
-    if (!pipe1_found) {
-      printf("Failed to find pipe 1\n");
+  if (pipe_count == 2) {
+    *pipe1 = pipes[0];
+    *pipe2 = pipes[1];
+  } else {
+    debug("Found %zu pipes at %zu, %zu (%c)\n", pipe_count, p.x, p.y,
+           input[idx(p.x, p.y, line_len)]);
+    for (size_t i = 0; i < pipe_count; i++) {
+      debug("  %c at %zu, %zu\n", pipes[i].pipe, pipes[i].pos.x,
+             pipes[i].pos.y);
     }
-    if (!pipe2_found) {
-      printf("Failed to find pipe 2\n");
-    }
-
-    assert(false);
-  }
-}
-
-Position follow(Position p, PipePosition target) {
-  switch (target.pipe) {
-  case '|':
-    if (target.pos.y > p.y) {
-      return (Position){.x = p.x, .y = p.y + 1};
-    } else {
-      return (Position){.x = p.x, .y = p.y - 1};
-    }
-  case '-':
-    if (target.pos.x > p.x) {
-      return (Position){.x = p.x + 1, .y = p.y};
-    } else {
-      return (Position){.x = p.x - 1, .y = p.y};
-    }
-  case 'L':
-    if (target.pos.x > p.x) {
-      return (Position){.x = p.x + 1, .y = p.y};
-    } else {
-      return (Position){.x = p.x, .y = p.y + 1};
-    }
-  case 'J':
-    if (target.pos.x < p.x) {
-      return (Position){.x = p.x - 1, .y = p.y};
-    } else {
-      return (Position){.x = p.x, .y = p.y + 1};
-    }
-  case '7':
-    if (target.pos.x < p.x) {
-      return (Position){.x = p.x - 1, .y = p.y};
-    } else {
-      return (Position){.x = p.x, .y = p.y - 1};
-    }
-  case 'F':
-    if (target.pos.y < p.y) {
-      return (Position){.x = p.x, .y = p.y - 1};
-    } else {
-      return (Position){.x = p.x + 1, .y = p.y};
-    }
-  case 'S':
-    return target.pos;
-  default:
-    printf("Invalid pipe: %x\n", target.pipe);
     assert(false);
   }
 }
@@ -190,7 +146,7 @@ int main(int argc, char *argv[]) {
   for (size_t i = 0; i < input_len; i++) {
     // Validate characters
     if (strchr(valid_map_chars, input[i]) == NULL && input[i] != '\n') {
-      printf("Invalid character in input: %c\n", input[i]);
+      debug("Invalid character in input: %c\n", input[i]);
       return EXIT_FAILURE;
     }
 
@@ -209,7 +165,7 @@ int main(int argc, char *argv[]) {
   Position start_pos = pos(start_index, line_len);
 
   // Walk the pipe
-  PipePosition pipe[1000] = {0};
+  PipePosition pipe[100000] = {0};
   size_t step = 0;
   pipe[0] = (PipePosition){.pos = start_pos, .pipe = 'S'};
   while (true) {
@@ -217,36 +173,36 @@ int main(int argc, char *argv[]) {
     PipePosition prev_pipe = pipe[step - 1];
     PipePosition pipe1 = {0}, pipe2 = {0};
 
-    printf("Step %zu: %zu, %zu\n", step, cur_pipe.pos.x, cur_pipe.pos.y);
+    debug("Step %zu: %zu, %zu\n", step, cur_pipe.pos.x, cur_pipe.pos.y);
 
     connected_pipes(input, input_len, line_len, cur_pipe.pos, &pipe1, &pipe2);
-    if (input[idx(cur_pipe.pos.x, cur_pipe.pos.y, line_len)] == 'S') {
+    if (cur_pipe.pipe == 'S') {
       if (step != 0) {
-        printf("Found start pipe at step %zu\n", step);
-        return EXIT_FAILURE;
+        debug("Found start pipe at step %zu\n", step);
+        break;
       }
 
       // Follow either pipe, so let's pick the first one
       Position next_pos = pipe1.pos;
       pipe[++step] = (PipePosition){.pos = next_pos, .pipe = pipe1.pipe};
 
-      printf("Following pipe %c\n", pipe1.pipe);
+      debug("Following pipe %c\n", pipe1.pipe);
     } else {
       // Follow the pipe that is not the one we came from
       if (pipe1.pos.x == prev_pipe.pos.x && pipe1.pos.y == prev_pipe.pos.y) {
         Position next_pos = pipe2.pos;
         pipe[++step] = (PipePosition){.pos = next_pos, .pipe = pipe2.pipe};
-        printf("Following pipe %c\n", pipe2.pipe);
+        debug("Following pipe %c\n", pipe2.pipe);
       } else {
         Position next_pos = pipe1.pos;
         pipe[++step] = (PipePosition){.pos = next_pos, .pipe = pipe1.pipe};
-        printf("Following pipe %c\n", pipe1.pipe);
+        debug("Following pipe %c\n", pipe1.pipe);
       }
     }
   }
 
-  printf("%s\n", input);
-  printf("%zu\n", start_index);
+  debug("Farthest away:");
+  printf("%zu\n", step / 2);
 
   free(input);
 
